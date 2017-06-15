c3797b5f63876603dffb2432795c7f3786088488
RichardLiK <ziyelianxuan@163.com>
2017-06-15 21:11:48 +0800

    Create ajaxAction.js

--- public/script/ajaxAction.js
+++ public/script/ajaxAction.js
@@ -26,7 +26,7 @@ function loadXMLDoc(filename, path){
 	    	myDivContent.innerHTML = xmlhttp.responseText;
 	    }
 	}
-	xmlhttp.open("POST",path + "/" + filename,true);
+	xmlhttp.open("GET",path + "/" + filename,true);
 	//设置ajax请求格式，通过send可以向后台传数据
 	//xmlhttp.open("GET","src/" + filename,true);
 	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
