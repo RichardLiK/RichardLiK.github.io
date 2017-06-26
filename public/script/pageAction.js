3b4a1af6bf26efb9cad9dac17c6c69b670fadd06
RichardLiK <ziyelianxuan@163.com>
2017-06-26 23:22:08 +0800

    disappear change

--- public/script/pageAction.js
+++ public/script/pageAction.js
@@ -2,13 +2,31 @@ function $(id){
 	return document.getElementById(id);
 }
 //实现welcome界面
-
-function contentSecChange(){
-
-	$('welcomeSec').style.display = 'none';
-	$('secContent').style.display = 'block';
+var welcomeSec = $('welcomeSec');
+welcomeSec.style.opacity = 1;
+//welcomeSec.style.display = 'none';//
+var secContent = $('secContent');
+//secContent.style.opacity = 0;//0
+//secContent.style.display = 'block';//
+function contentSecChange(ele1, ele2, interval){
+	clearInterval(interval);
+	ele1.style.display = 'none';
+	ele2.style.display = 'block';
 }
-var t = setTimeout("contentSecChange()", 2000);
+function opaChange(ele, method){
+	if(method == 'plus'){
+		ele.style.opacity += 0.03;	
+	} else if(method == 'subtraction'){
+		ele.style.opacity -= 0.03;
+	}
+	
+} 
+//opaChange(secContent, 'plus');
+var welOpacityChange = setInterval("opaChange(welcomeSec, 'subtraction')", 100);
+var welcomeDisappear = setTimeout("contentSecChange(welcomeSec, secContent, welOpacityChange)", 2000);
+//这里加只能加一次，即变成0.03，不知道什么原因
+// var conOpacityChange = setInterval("opaChange(secContent, 'plus')", 100);
+// var welcomeDisappear = setTimeout("contentSecChange(welcomeSec, secContent, conOpacityChange)", 2000);
 
 //四格li透明度递减
 var secContent = document.getElementById('secContent');
